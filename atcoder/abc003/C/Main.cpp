237626fa6acb6b741cb5d8ed8717554d32bb496d
musaprg <conie112@gmail.com>
2017-08-19 22:56:09 +0900

    modified

--- atcoder/abc003/C/Main.cpp
+++ atcoder/abc003/C/Main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
-#define max(A,B) A>B ? A : B
 
 using namespace std;
 
 int N,K;
 vector<double> R;
-double dp[100][100]; //dp[i][j]: i番目以降から
-
-double rec(int i, int k){
-    if(i<N && k>=0){
-        cout<<i<<endl;
-        return max((rec(i+1,k-1)+R[i])/2, rec(i+1,k));
-    }else return 0;
-}
 
 int main(void){
     cin.sync_with_stdio(false);
@@ -23,6 +15,12 @@ int main(void){
         int n; cin>>n;
         R.push_back(n);
     }
-    cout<<rec(0, K)<<endl;
+    sort(R.begin(), R.end());
+    // vector<double>::iterator ir = R.begin();
+    double ans = 0;
+    for(int i = R.size()-K; i < R.size(); i++){
+        ans += R[i]; ans /= 2;
+    }
+    printf("%.6f\n", ans);
     return 0;
 }
