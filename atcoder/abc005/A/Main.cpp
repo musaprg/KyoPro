bacb4f834b5a745037f5438dd8b47e14e1dc5945
musaprg <conie112@gmail.com>
2017-08-24 00:10:33 +0900

    modifed

---
+++ atcoder/abc005/A/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    cin.sync_with_stdio(false);
+    int x,y; cin>>x>>y;
+    cout<<y/x<<endl;
+    return 0;
+}
