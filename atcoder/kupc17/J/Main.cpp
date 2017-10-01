29194d88d12f51438b5d7d8bf57a327ff6d9f32b
musaprg <conie112@gmail.com>
2017-10-01 12:42:20 +0900

    modified

---
+++ atcoder/kupc17/J/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long int
+
+using namespace std;
+
+int main(void){
+    cin.sync_with_stdio(false);
+    int N;
+    cin>>N;
+    return 0;
+}
