b782723693f758738b09569ea47bee742adeaf9c
musaprg <conie112@gmail.com>
2018-01-28 20:18:23 +0900

    update

---
+++ atcoder/abc087/B/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector> //可変長配列
+#include <queue> //キュー
+#include <unordered_map> //ハッシュ連想配列
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#define ll long long int
+#define INFTY (1<<21)
+
+using namespace std;
+
+int main(void){
+    cin.sync_with_stdio(false);
+    int N;
+    cin>>N;
+    return 0;
+}
